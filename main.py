6c1153a46acdf53279d94b845b6a4e25264c91b9
Scrodinger <schrodinger.grigorii@gmail.com>
2023-06-29 01:59:19 +0300

    update

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-import time
-from yeelight import Bulb
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager
 
-bulb = Bulb("192.168.0.238")
+from src.screens.lamps_screen.lamps_screen import Screen1
+from src.screens.main_screen.main_screen import MainScreen
+from src.screens.music_screen.music_screen import Screen3
+from src.screens.scenario_screen.scenario_screen import Screen2
 
-for x in range(10):
-    bulb.turn_on(effect="sudden")
-    time.sleep(0.3)
-    bulb.turn_off()
-    time.sleep(0.3)
 
+class MyApp(App):
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(MainScreen(name='main'))
+        sm.add_widget(Screen1(name='screen1'))
+        sm.add_widget(Screen2(name='screen2'))
+        sm.add_widget(Screen3(name='screen3'))
+
+        return sm
+
+
+if __name__ == '__main__':
+    MyApp().run()
--- src/screens/main_screen/main_screen.py
+++ src/screens/main_screen/main_screen.py
@@ -1,10 +1,5 @@
-from kivy.app import App
 from kivy.uix.button import Button
-from kivy.uix.screenmanager import ScreenManager, Screen
-
-from src.screens.lamps_screen.lamps_screen import Screen1
-from src.screens.music_screen.music_screen import Screen3
-from src.screens.scenario_screen.scenario_screen import Screen2
+from kivy.uix.screenmanager import Screen
 
 
 class MainScreen(Screen):
@@ -33,17 +28,3 @@ class MainScreen(Screen):
     def go_to_screen3(self, instance):
         self.manager.current = 'screen3'
 
-
-class MyApp(App):
-    def build(self):
-        sm = ScreenManager()
-        sm.add_widget(MainScreen(name='main'))
-        sm.add_widget(Screen1(name='screen1'))
-        sm.add_widget(Screen2(name='screen2'))
-        sm.add_widget(Screen3(name='screen3'))
-
-        return sm
-
-
-if __name__ == '__main__':
-    MyApp().run()
