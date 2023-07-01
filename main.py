e9f73b39c2c4378acd9baf1a1183e7815ac1d95d
Scrodinger <schrodinger.grigorii@gmail.com>
2023-07-01 14:29:22 +0300

    new app

--- main.py
+++ main.py
@@ -29,40 +29,40 @@ This demonstrates the use of a mesh mode to distort an image. You should see
 a line of buttons across the bottom of a canvas. Pressing them displays
 the mesh, a small circle of points, with different mesh.mode settings.
 '''
-import threading
-import time
-
 # import threading
+# import time
 
 from kivy.app import App
-from kivy.clock import Clock
+# from kivy.clock import Clock
 from kivy.uix.button import Button
-
-from kivy.uix.gridlayout import GridLayout
 # from yeelight import Bulb
 
 
 class SimpleApp(App):
     def build(self):
-        layout = GridLayout(cols=2)
-        layout.add_widget(Button(text='RUN!!', on_release=self.execute_function))
-
-        return layout
-
-    def execute_function(self, instance):
-        Clock.schedule_once(lambda dt: self.execute_function_thread(), 0)
-
-    def execute_function_thread(self):
-        threading.Thread(target=self.execute_function_thread_helper).start()
+        btn1 = Button(text='Hello world 1')
+        btn1.bind(on_press=self.callback)
+        btn2 = Button(text='Hello world 2')
+        btn2.bind(on_press=self.callback)
 
     @staticmethod
-    def execute_function_thread_helper():
-        # bulb = Bulb("192.168.0.238")
-        for x in range(10):
-            # bulb.turn_on(effect="sudden")
-            time.sleep(0.3)
-            # bulb.turn_off()
-            time.sleep(0.3)
+    def callback(instance):
+        print('The button <%s> is being pressed' % instance.text)
+
+    # def execute_function(self, instance):
+    #     Clock.schedule_once(lambda dt: self.execute_function_thread(), 0)
+    #
+    # def execute_function_thread(self):
+    #     threading.Thread(target=self.execute_function_thread_helper).start()
+    #
+    # @staticmethod
+    # def execute_function_thread_helper():
+    #     bulb = Bulb("192.168.0.238")
+    #     for x in range(10):
+    #         bulb.turn_on(effect="sudden")
+    #         time.sleep(0.3)
+    #         bulb.turn_off()
+    #         time.sleep(0.3)
 
 
 if __name__ == "__main__":
