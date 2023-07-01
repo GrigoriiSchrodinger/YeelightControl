ab967fd37c04e46faf1834504c90f99c5b044419
Scrodinger <schrodinger.grigorii@gmail.com>
2023-07-01 11:58:41 +0300

    New app

--- main.py
+++ main.py
@@ -1,22 +1,80 @@
+# from kivy.app import App
+# from kivy.uix.screenmanager import ScreenManager
+#
+# from src.screens.lamps_screen.lamps_screen import Screen1
+# from src.screens.main_screen.main_screen import MainScreen
+# from src.screens.music_screen.music_screen import Screen3
+# from src.screens.scenario_screen.scenario_screen import Screen2
+#
+#
+# class MyApp(App):
+#     def build(self):
+#         sm = ScreenManager()
+#         sm.add_widget(MainScreen(name='main'))
+#         sm.add_widget(Screen1(name='screen1'))
+#         sm.add_widget(Screen2(name='screen2'))
+#         sm.add_widget(Screen3(name='screen3'))
+#
+#         return sm
+#
+#
+# if __name__ == '__main__':
+#     MyApp().run()
+
+'''
+Mesh test
+=========
+
+This demonstrates the use of a mesh mode to distort an image. You should see
+a line of buttons across the bottom of a canvas. Pressing them displays
+the mesh, a small circle of points, with different mesh.mode settings.
+'''
+
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager
-
-from src.screens.lamps_screen.lamps_screen import Screen1
-from src.screens.main_screen.main_screen import MainScreen
-from src.screens.music_screen.music_screen import Screen3
-from src.screens.scenario_screen.scenario_screen import Screen2
+from kivy.graphics import Mesh
+from functools import partial
+from math import cos, sin, pi
 
 
-class MyApp(App):
+class MeshTestApp(App):
+
+    def change_mode(self, mode, *largs):
+        self.mesh.mode = mode
+
+    def build_mesh(self):
+        """ returns a Mesh of a rough circle. """
+        vertices = []
+        indices = []
+        step = 10
+        istep = (pi * 2) / float(step)
+        for i in range(step):
+            x = 300 + cos(istep * i) * 100
+            y = 300 + sin(istep * i) * 100
+            vertices.extend([x, y, 0, 0])
+            indices.append(i)
+        return Mesh(vertices=vertices, indices=indices)
+
     def build(self):
-        sm = ScreenManager()
-        sm.add_widget(MainScreen(name='main'))
-        sm.add_widget(Screen1(name='screen1'))
-        sm.add_widget(Screen2(name='screen2'))
-        sm.add_widget(Screen3(name='screen3'))
+        wid = Widget()
+        with wid.canvas:
+            self.mesh = self.build_mesh()
 
-        return sm
+        layout = BoxLayout(size_hint=(1, None), height=50)
+        for mode in ('points', 'line_strip', 'line_loop', 'lines',
+                'triangle_strip', 'triangle_fan'):
+            button = Button(text=mode)
+            button.bind(on_release=partial(self.change_mode, mode))
+            layout.add_widget(button)
+
+        root = BoxLayout(orientation='vertical')
+        root.add_widget(wid)
+        root.add_widget(layout)
+
+        return root
 
 
 if __name__ == '__main__':
-    MyApp().run()
+    MeshTestApp().run()
