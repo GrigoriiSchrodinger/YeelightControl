04b6d26654176e15950215467146694d5044efa1
Scrodinger <schrodinger.grigorii@gmail.com>
2023-07-01 16:01:28 +0300

    f

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import logging
-
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
@@ -7,34 +5,26 @@ from yeelight import Bulb
 
 
 class MainPage(App):
-    logging.basicConfig(level=logging.DEBUG,
-                        filename='app.log',
-                        format='%(asctime)s [%(levelname)s]: %(message)s')
-
     @staticmethod
     def run_s():
-        try:
-            bulb = Bulb("192.168.0.238")
-            bulb.turn_on(effect="sudden")
-        except Exception as e:
-            logging.exception("Произошла ошибка: %s", str(e))
+        times = 0
+        print(times + 33)
+        # bulb = Bulb("192.168.0.238")
+        # bulb.turn_on(effect="sudden")
 
     def execute_function(self, instance):
         self.run_s()
 
     def build(self):
-        try:
-            button_run = Button(text="run", on_release=self.execute_function)
+        button_run = Button(text="run", on_release=self.execute_function)
 
-            layout = BoxLayout(size_hint=(1, None), height=50)
-            layout.add_widget(button_run)
+        layout = BoxLayout(size_hint=(1, None), height=50)
+        layout.add_widget(button_run)
 
-            root = BoxLayout(orientation='vertical')
-            root.add_widget(layout)
+        root = BoxLayout(orientation='vertical')
+        root.add_widget(layout)
 
-            return root
-        except Exception as e:
-            logging.exception("Произошла ошибка: %s", str(e))
+        return root
 
 
 if __name__ == '__main__':
