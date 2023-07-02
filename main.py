e30c5d5420c54e68a21476caf92c94833d7462de
Scrodinger <schrodinger.grigorii@gmail.com>
2023-07-03 01:53:01 +0300

    add 120 fps

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from kivy import Config
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
 
 from src.screens.main_screen.lamps_screen.lamps_screen import LampsScreen
 from src.screens.main_screen.main_screen import MainScreen
 from src.screens.main_screen.scenario_screen.scenario_screen import ScenarioScreen
+
+Config.set('graphics', 'maxfps', '120')
 
 
 class MyApp(App):
